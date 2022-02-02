61766e5bbed11fbeb841e94c7c2bb06f679a461e
mayuripujari <74092363+mayuripujari@users.noreply.github.com>
2022-02-02 10:53:31 +0530

    initial commit in math.py file

---
+++ math.py
@@ -0,0 +1,12 @@
+# Add implementation
+def add(x,y):
+    pass
+# Subtract implementation
+def subtract(x,y):
+    pass
+# multiply implementation
+def multiply(x,y):
+    pass
+
+def divide(x,y):
+    pass
